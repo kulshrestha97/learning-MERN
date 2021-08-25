89e2112efb425e5644c08b8a5b53fee96d09052d
Rajat Kulshreshtha <rajat.kulshreshtha@hexagon.com>
2021-08-25 22:47:49 +0530

    added user routes, didn't add express-validation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // It is a good practice to first import the core modules and system modules then leave a line and then import the user made modules
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const {check} = require('express-validator')
 const placesRoutes = require('./routes/places-routes');
 
 
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use('/api/places',placesRoutes);
-
+app.use('/api/users', userRoutes);
 // This is a special middleware that is recognised by express because of the first argument 'error'. 
 app.use((error, req, res, next) => {
     if(res.headerSent)
