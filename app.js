347691242b7ac7dd40f4d34eac420b570df032bc
Rajat Kulshreshtha <rahul.kulshrestha97@gmail.com>
2021-08-29 13:59:16 +0530

    commit changes

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const controller = require('./mongo');
+const express = require("express");
+const bodyParser = require("body-parser");
+const controller = require("./mongo");
+const url =
+  "mongodb+srv://kulshrestha97:Password_123@cluster0.w9hnd.mongodb.net/places?retryWrites=true&w=majority";
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.post("/products", controller.createProduct);
 
-app.post('/products', controller.createProduct);
+app.get("/products", controller.getProducts);
 
-app.get('/products');
-
-app.listen(5000);
+mongoose
+  .connect(url)
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
