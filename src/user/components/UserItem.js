a638a83b879513911bec9d08f6f1d3419fc8f77d
Rajat Kulshreshtha <rahul.kulshrestha97@gmail.com>
2021-08-29 19:59:53 +0530

    Changes in frontend

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React from "react";
+import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
+import "./UserItem.css";
 
-import './UserItem.css';
-
-const UserItem = props => {
+const UserItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
+      <Card className="user-item__content">
         <div className="user-item__image">
-          <img src={props.image} alt={props.name} />
+          <Avatar image={props.image} alt={props.name} />
         </div>
         <div className="user-item__info">
           <h2>{props.name}</h2>
           <h3>
-            {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
+            {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
           </h3>
         </div>
-      </div>
+      </Card>
     </li>
   );
 };
